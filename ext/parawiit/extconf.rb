788f351f267b192d8623f4b4cbf896a8b0f7dd2d
Eric Cranston <cracell@gmail.com>
2012-02-20 14:48:57 -0800

    Moved into OS X directory

--- ext/parawiit/extconf.rb
+++ ext/parawiit/extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 if RUBY_PLATFORM =~ /darwin/ then
   $LDFLAGS << ' -framework IOBluetooth'
 
-  create_makefile 'parawiit/parawiit'
+  create_makefile 'parawiit/parawiit', 'macosx'
 
   open 'Makefile', 'a' do |io|
     io.write "\n.m.o:\n\t#{COMPILE_C}\n\n"
